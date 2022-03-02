8bc9849b42989c860361c18d43d259ddfe150164
Lucival dos Santos França Filho <civalfran1@gmail.com>
2022-03-01 22:15:37 -0300

    fix: card style

--- src/pages/challenges/index.tsx
+++ src/pages/challenges/index.tsx
@@ -1,27 +1,22 @@
 import React from 'react'
 import Layout from '_components/layout';
 import challenges from '_data/challenges.json';
+import Card from '_components/card'
+import styles from './style.module.css'
 
 export default function Challenges() {
   return (
-    <Layout headTitle='Challenge' >
-      <h1 className={'title'}>
-          Challenges
-      </h1>
-      {
-        challenges.map(({ id, title, level, description, example, tip = false }) => (
-          <div className='card'>
-            <h2>Desafio {id}: {title}</h2>
-            <p>{level}</p>
-            <p>{description}</p>
-            <div>
-              <p>{example.input}</p>
-              <p>{example.output}</p>
-            </div>
-            {!tip ? (<p>sem dicas</p>) : (<p>Dica: {tip}</p>)}
-          </div>
-        ))
-      }
+    <Layout headTitle='Challenges' >
+      <main className={styles.main}>
+        <h1 className={'title'}>
+            Challenges
+        </h1>
+        {
+          challenges.map((challenge) => (
+            <Card {...challenge} />
+          ))
+        }
+      </main>
     </Layout>
   )
 }
